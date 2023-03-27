ac6f1f9c02c562816a68f2ac42e4eaaae285b7b0
Paul Maslov <pavel.masloff@gmail.com>
2023-03-27 22:06:22 +0200

    Update MyLambda.swift

--- Sources/MyLambda/MyLambda.swift
+++ Sources/MyLambda/MyLambda.swift
@@ -4,7 +4,6 @@ import AWSLambdaEvents
 
 @main
 struct MyLambda: SimpleLambdaHandler {
-    // In this example we are receiving and responding with `Codable`.
     func handle(_ request: APIGatewayV2Request, context: LambdaContext) async throws -> APIGatewayV2Response {
         return APIGatewayV2Response(statusCode: .ok, body: "hello world")
     }
